343900854b2808bddf6247f112c572a4935c4093
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2022-08-28 11:20:01 -0500

    correcciones de apis de fases

--- app/Http/Controllers/FasesController.php
+++ app/Http/Controllers/FasesController.php
@@ -10,16 +10,22 @@ class FasesController extends Controller
     public function insertarGanadoresEstatal(Request $request)
     {
         try {
-            $calificaciones = $request;
+            $calificaciones = $request->calificaciones;
             $sedes = ['El Mante', 'Victoria', 'Reynosa', 'Madero', 'Nuevo Laredo', 'Matamoros'];
             $categorias = ['petit', 'kids', 'juvenil', 'media_superior', 'superior', 'posgrado'];
             foreach ($sedes as &$sede) {
                 foreach ($categorias as &$categoria) {
                     foreach ($calificaciones[$sede][$categoria] as &$proyecto) {
-                        $this->registrarGanadoresEstatal($proyecto);
+                        if ($proyecto != null) {
+                            $this->registrarGanadoresEstatal($proyecto);
+                        }
                     }
                 }
             }
+            return response()->json([
+                'error' => false,
+                'msg' => 'se activo correctamente la fase estatal',
+            ]);
         } catch (\Exception$th) {
             return response()->json([
                 'error' => true,
@@ -30,13 +36,19 @@ class FasesController extends Controller
     public function insertarGanadoresInternacional(Request $request)
     {
         try {
-            $calificaciones = $request;
+            $calificaciones = $request->calificaciones;
             $categorias = ['petit', 'kids', 'juvenil', 'media_superior', 'superior', 'posgrado'];
             foreach ($categorias as &$categoria) {
                 foreach ($calificaciones['Estatal'][$categoria] as &$proyecto) {
-                    $this->registrarGanadoresInternacional($proyecto);
+                    if ($proyecto != null) {
+                        $this->registrarGanadoresInternacional($proyecto);
+                    }
                 }
             }
+            return response()->json([
+                'error' => false,
+                'msg' => 'se activo correctamente la fase internacional',
+            ]);
         } catch (\Exception$th) {
             return response()->json([
                 'error' => true,
@@ -46,17 +58,18 @@ class FasesController extends Controller
     }
     private function registrarGanadoresEstatal($proyecto)
     {
-        $proyecto = Proyecto::where('id', $proyecto->id_proyectos)->first();
+        $proyecto = Proyecto::where('id', $proyecto['id_proyectos'])->first();
         $proyectoNuevo = new Proyecto();
-        $proyectoNuevo->idparticipante = $proyecto->idparticipante;
-        $proyectoNuevo->modalidad = $proyecto->modalidad;
+        $proyectoNuevo->idparticipante = $proyecto['idparticipante'];
+        $proyectoNuevo->modalidad = $proyecto['modalidad'];
         $proyectoNuevo->sede = 'Estatal';
-        $proyectoNuevo->urlvideo = $proyecto->urlvideo;
-        $proyectoNuevo->categoria = $proyecto->categoria;
-        $proyectoNuevo->titulo = $proyecto->titulo;
-        $proyectoNuevo->descripcion = $proyecto->descripcion;
-        $proyectoNuevo->area = $proyecto->area;
-        $proyectoNuevo->activo_foto = $proyecto->activo_foto;
+        $proyectoNuevo->urlvideo = $proyecto['urlvideo'];
+        $proyectoNuevo->categoria = $proyecto['categoria'];
+        $proyectoNuevo->titulo = $proyecto['titulo'];
+        $proyectoNuevo->descripcion = $proyecto['descripcion'];
+        $proyectoNuevo->area = $proyecto['area'];
+        $proyectoNuevo->activo_foto = $proyecto['activo_foto'];
+        $proyectoNuevo->foto = $proyecto['foto'];
         $proyectoNuevo->save();
     }
     private function registrarGanadoresInternacional($proyecto)
